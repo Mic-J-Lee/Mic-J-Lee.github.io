f7e06a50358db279e85649446efaa2f98f8343e7
Michael Lee <mleenavy@gmail.com>
2017-05-09 14:00:59 -0500

    worst game v1

---
+++ pizza_restaurant.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  var address = '500 E Madison St Chicago';
+  $('#restaurants').before(address);
+
+  function blinker() {
+      $('.blink_me').fadeOut(100);
+      $('.blink_me').fadeIn(100);
+      var color = ['red', 'blue', 'green']
+      $('.blink_me').css({ 'color': color[Math.floor(Math.random() * color.length)]})
+  }
+
+  function win() {
+    if ($('.hasAddress').length === 0) {
+      console.log("You won!")
+      window.location.href = "bootstrap-turtles.html";
+    }
+  }
+  setInterval(win, 100)
+
+  function loss() {
+    $('h2').text("Too slow.")
+    console.log("You lost.")
+    window.location.href = "game_over.html"
+  }
+  setTimeout(loss, 15000)
+
+  function seizure() {
+    $('.container').css({"background": "url('https://media.giphy.com/media/hmzAcor7gBsbK/giphy.gif')"})
+
+    setInterval(blinker, 200);
+    recursiveCountDown()
+  }
+  setTimeout(seizure, 5000)
+
+  function recursiveCountDown(counter = 10) {
+    if (counter > 0) {
+      $('h2').text(`${counter}`)
+      counter --
+      setTimeout(recursiveCountDown.bind(null, counter), 1000)
+    }
+  }
+
+})
+
+function removeRestaurant() {
+  $('#restaurants').find('li').last().remove()
+}
+
---
+++ restaurant_finder.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+  $('button').on('click', function() {
+    $(this).parent().after( $(this).parent().data("address") )
+  })
+
+  $('.restaurant-detail').on('mouseenter', function() {
+    $(this).append( $('<img width="50px" src="https://33.media.tumblr.com/tumblr_mc23lmfLZ71rfjowdo1_500.gif">') )
+  })
+
+  $('.show-price').on('click', function(event) {
+    $(this).parent().find('.list-group').css('color', 'black')
+    event.preventDefault()
+    $(this).parent().find('.list-group').fadeToggle()
+  })
+
+  function win() {
+    if ($('img').length > 50) {
+      console.log("You won!")
+      window.location.href = "pizza_restaurant.html";
+    }
+  }
+  setInterval(win, 100)
+  
+  function loss() {
+    $('h2').text("Too slow.")
+    console.log("You lost.")
+    window.location.href = "game_over.html"
+  }
+  setTimeout(loss, 20000)
+
+  function seizure() {
+    $('.container').css({'background': "url('http://animatedpizzagifs.com/images/psycadelic-pizza-love.gif')"})
+    $('.row').css({'color': 'white'})
+    recursiveCountDown()
+  }
+  setTimeout(seizure, 10000)
+
+  function recursiveCountDown(counter = 10) {
+    if (counter > 0) {
+      $('h2').text(`${counter}`)
+      counter --
+      setTimeout(recursiveCountDown.bind(null, counter), 1000)
+    }
+  }
+})
+
+  
+
